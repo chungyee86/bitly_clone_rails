07500b02d64db35ae99278575a9b18bd322fdbaa
chungyee86 <buer5011@yahoo.com>
2017-03-17 03:21:55 -0700

    shorten of url done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'urls#index' #root page
+  get 'urls/:id/short' => 'urls#short'
+
 
   resources :urls
 
